792cde0781aa673bfb3a6ad6a71e3d80e371391b
SkkiKya <koya.sakaki@gmail.com>
2020-07-04 15:26:48 +0900

    merge

--- shop/cart.php
+++ shop/cart.php
@@ -8,13 +8,14 @@ $pdo = connect_db();
 if(!isset($_SESSION['cart'])){
   $_SESSION['cart'] = array();
 }
+
 if(@$_POST['submit']){
   @$_SESSION['cart'][$_POST['id']] += $_POST['num'];
 }
 
 foreach($_SESSION['cart'] as $code => $num) {
-  $stmt = $pdo->prepare("SELECT * FROM connect_db INNER JOIN items ON cnnect_db.item_id AND items.id");
-  $stmt->execute(array($code));
+  $st = $pdo->prepare("SELECT * FROM items WHERE id=?");
+  $st->execute(array($code));
   $row = $st->fetch();
   // print_r($st);
   // print_r($row);
